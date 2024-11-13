70c275a3dc960d94525a09aaaf9aceffcaf51751
DFafnir <davidchaguendo81@gmail.com>
2024-11-13 18:52:50 -0500

    Update java_carrito.js

--- assets/js/java_carrito.js
+++ assets/js/java_carrito.js
@@ -6,119 +6,119 @@ const baseDeDatos = [
         precio:180.000,
         imagen:'../img/accesorio1.png'
 
-    }
+    },
     {
         id:2,
         nombre:COLGANTELADIESSENCHAPADOENORO4K,
         precio:90.000,
         imagen:'../img/accesorio2.png'
 
-    }
+    },
     {
         id:3,
         nombre:COLLARDEPERLASREALESLADIES,
         precio:100.000,
         imagen:'../img/accesorio3.png'
 
-    }
+    },
     {
         id:4,
         nombre:ANILLOBLACKLADIESPIEDRAPRECIOSAUNITALLA,
         precio:200.000,
         imagen:'../img/accesorio4.png'
 
-    }
+    },
     {
         id:5,
         nombre:BRAZALETEDEPERLASCUTELADIES,
         precio:50.000,
         imagen:'../img/accesorio5.png'
 
-    }
+    },
     {
         id:6,
         nombre:CHERRYREDBOOTSLADIESENCUERO,
         precio:350.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:7,
         nombre:MOCASINESDEDAMAENNEGRO,
         precio:260.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:8,
         nombre:TACONESCONPLATAFORMADEDAMASHOELADIES,
         precio:200.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:9,
         nombre:BOTINESLADIESENCUEROMARRÓN,
         precio:450.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:10,
-        nombre:MOCASINESDEPUNTAREDONDALADIES´S,
+        nombre:MOCASINESDEPUNTAREDONDALADIESS,
         precio:350.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:11,
         nombre:CAMISAOVERSIZEELEMENTVICTORIANA,
         precio:30.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:12,
         nombre:CAMISAOVERSIZEGENTLEMANVICTORIANA,
         precio:30.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:13,
         nombre:CORSEtVICTORIANOPARADAMAENBEIGECLÁSICO,
         precio:69.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:14,
         nombre:LADIESDRESSENBEIGEYFLORES,
         precio:90.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:15,
         nombre:GABARDINACLÁSICAENBEIGE,
         precio:200.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:16,
         nombre:SWEETBRONWPANTALONESCLÁSICOSRECTOS,
         precio:50.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:17,
         nombre:LADIESPRETTYDRESSENBEIGE,
         precio:90.000,
         imagen:'../img/calzado1.png'
 
-    }
+    },
     {
         id:18,
         nombre:TOTEPRETTYBAGLADIESENBEIGE,
