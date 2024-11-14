8f9ab7bd5f2a50584a3aecb9af9a0fc921997194
DFafnir <davidchaguendo81@gmail.com>
2024-11-14 11:15:03 -0500

    final

---
+++ assets/js/productos.js
@@ -0,0 +1,93 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const productos = [
+    {
+      id:1,
+      nombre: "RELOJ ENCHAPADO EN ORO DE 18K VINTAGE",
+      precio: 180000,
+    },
+    {
+      id:2,
+      nombre: "COLGANTE LADIES´S ENCHAPADO EN ORO 4K",
+      precio: 90000,
+    },
+    {
+      id:3,
+      nombre: "COLLAR DE PERLAS REALES LADIES´S",
+      precio: 100000,
+    },
+    {
+      id:4,
+      nombre: "ANILLO BLACK LADIES´S PIEDRA PRECIOSA UNITALLA",
+      precio: 200000,
+    },
+    {
+      id:5,
+      nombre: "BRAZALETE DE PERLAS CUTE LADIES´S",
+      precio: 50000,
+    },
+    {
+      id:6,
+      nombre: "CHERRY RED BOOTS LADIES´S EN CUERO",
+      precio: 350000,
+    },
+    {
+      id:7,
+      nombre: "MOCASINES DE DAMA EN NEGRO",
+      precio: 260000
+    },
+    {
+      id:8,
+      nombre: "TACONES CON PLATAFORMA DE DAMA SHOE LADIES´S",
+      precio: 200000
+    },
+    {
+      id:9,
+      nombre: "BOTINES LADIES´S EN CUERO MARRÓN",
+      precio: 450000
+    },
+    {
+      id:10,
+      nombre:"MOCASINES DE PUNTA REDONDA LADIES´S",
+      precio: 350000
+    },
+    {
+      id: 11,
+      nombre: "CAMISA OVERSIZE ELEMENTS VICTORIANA",
+      precio: 30000
+    },
+    {
+      id:12,
+      nombre: "CAMISA OVERSIZE GENTLEMAN VICTORIANA",
+      precio: 30000
+    },
+    {
+      id:13,
+      nombre: "CORSET VICTORIANO PARA DAMA EN BEIGE CLÁSICO",
+      precio: 69000
+    },
+    {
+      id: 14,
+      nombre: "LADIES´S DRESS EN BEIGE Y FLORES",
+      precio: 90000
+    },
+    {
+      id: 15,
+      nombre: "GABARDINA CLÁSICA EN BEIGE",
+      precio: 200000
+    },
+    {
+      id: 16,
+      nombre: "SWEET BRONW PANTALONES CLÁSICOS RECTOS",
+      precio: 50000
+    },
+    {
+      id: 17,
+      nombre: "LADIES´S PRETTY DRESS EN BEIGE",
+      precio: 90000
+    },
+    {
+      id:18,
+      nombre: "TOTE PRETTY BAG LADIES´S EN BEIGE",
+      precio: 15000
+    }
+  ]
