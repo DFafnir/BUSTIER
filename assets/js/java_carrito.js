d5c73ea14a418babfd911a23e78b0435bb4e7518
Vanessa1209 <vanguza6@gmail.com>
2024-11-13 19:31:34 -0500

    javamaldito

--- assets/js/java_carrito.js
+++ assets/js/java_carrito.js
@@ -1,129 +1,232 @@
 //PRODUCTOS DE BUSTIER
-const baseDeDatos = [
+const productos = [
+    //Accesorios
     {
         id:1,
-        nombre:RELOJ_ENCHAPADO_EN_ORO_DE_18K_VINTAGE,
+        titulo:"RELOJ",
         precio:180.000,
-        imagen:'../img/accesorio1.png'
+        imagen:'./assets/img_productos/accesorio1.png',
+        categoria: {
+            nombre:"Accesorios",
+            id:"Accesorios"
+            }
 
-    }
+    },
     {
         id:2,
-        nombre:COLGANTELADIESSENCHAPADOENORO4K,
+        titulo:"COLGANTE",
         precio:90.000,
-        imagen:'../img/accesorio2.png'
+        imagen:'./assets/img_productos/accesorio2.png',
+        categoria:{
+            nombre:"Accesorios",
+            id:"Accesorios"
+        }
 
-    }
+    },
     {
         id:3,
-        nombre:COLLARDEPERLASREALESLADIES,
+        titulo:"COLLAR",
         precio:100.000,
-        imagen:'../img/accesorio3.png'
+        imagen:'./assets/img_productos/accesorio3.png',
+        categoria:{
+            nombre:"Accesorios",
+            id:"Accesorios"
+        }
 
-    }
+    },
     {
         id:4,
-        nombre:ANILLOBLACKLADIESPIEDRAPRECIOSAUNITALLA,
+        titulo:"ANILLO",
         precio:200.000,
-        imagen:'../img/accesorio4.png'
+        imagen:'./assets/img_productos/accesorio4.png',
+        categoria:{
+            nombre:"Accesorios",
+            id:"Accesorios"
+        }
 
-    }
+    },
     {
         id:5,
-        nombre:BRAZALETEDEPERLASCUTELADIES,
+        titulo:"BRAZALETE",
         precio:50.000,
-        imagen:'../img/accesorio5.png'
-
-    }
+        imagen:'./assets/img_productos/accesorio5.png',
+        categoria:{
+            nombre:"Accesorios",
+            id:"Accesorios"
+        }
+//CALZADO
+    },
     {
         id:6,
-        nombre:CHERRYREDBOOTSLADIESENCUERO,
+        titulo:"CHERRY",
         precio:350.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/calzado1.png',
+        categoria:{
+            nombre:"Calzado",
+            id:"Calzado"
+        }
 
-    }
+    },
     {
         id:7,
-        nombre:MOCASINESDEDAMAENNEGRO,
+        titulo:"MOCASINES",
         precio:260.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/calzado2.png',
+        categoria:{
+            nombre:"Calzado",
+            id:"Calzado"
+        }
 
-    }
+
+    },
     {
         id:8,
-        nombre:TACONESCONPLATAFORMADEDAMASHOELADIES,
+        titulo:"TACONES",
         precio:200.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/calzado3.png',
+        categoria:{
+            nombre:"Calzado",
+            id:"Calzado"
+        }
 
-    }
+
+    },
     {
         id:9,
-        nombre:BOTINESLADIESENCUEROMARRÓN,
+        titulo:"BOTINES",
         precio:450.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/clazado4.png',
+        categoria:{
+            nombre:"Calzado",
+            id:"Calzado"
+        }
 
-    }
+
+    },
     {
         id:10,
-        nombre:MOCASINESDEPUNTAREDONDALADIES´S,
+        titulo:"MOCASINESMARRONES",
         precio:350.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/clazado5.png',
+        categoria:{
+            nombre:"Calzado",
+            id:"Calzado"
+        }
+//ROPA
 
-    }
+    },
     {
         id:11,
-        nombre:CAMISAOVERSIZEELEMENTVICTORIANA,
+       titulo:"CAMISA1",
         precio:30.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/oversize elements.png',
+        categoria:{
+            nombre:"Prendas",
+            id:"Prendas"
+        }
 
-    }
+
+    },
     {
         id:12,
-        nombre:CAMISAOVERSIZEGENTLEMANVICTORIANA,
+        titulo:"CAMISA2",
         precio:30.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/oversize gentleman.png',
+        categoria:{
+            nombre:"Prendas",
+            id:"Prendas"
+        }
 
-    }
+    },
     {
         id:13,
-        nombre:CORSEtVICTORIANOPARADAMAENBEIGECLÁSICO,
+        titulo:"CORSET",
         precio:69.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/prenda1.png',
+        categoria:{
+            nombre:"Prendas",
+            id:"Prendas"
+        }
 
-    }
+    },
     {
         id:14,
-        nombre:LADIESDRESSENBEIGEYFLORES,
+        titulo:"DRESS",
         precio:90.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/prenda2.png',
+        categoria:{
+            nombre:"Prendas",
+            id:"Prendas"
+        }
 
-    }
+    },
     {
         id:15,
-        nombre:GABARDINACLÁSICAENBEIGE,
+        titulo:"GABARDINA",
         precio:200.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/prenda3.png',
+        categoria:{
+            nombre:"Prendas",
+            id:"Prendas"
+        }
 
-    }
+    },
     {
         id:16,
-        nombre:SWEETBRONWPANTALONESCLÁSICOSRECTOS,
+        titulo:"PANTALONES",
         precio:50.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/prenda4.png',
+        categoria:{
+            nombre:"RPrendas",
+            id:"Prendas"
+        }
 
-    }
+    },
     {
         id:17,
-        nombre:LADIESPRETTYDRESSENBEIGE,
+        titulo:"LADIES",
         precio:90.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/prenda5.png',
+        categoria:{
+            nombre:"Prendas",
+            id:"Prendas"
+        }
 
-    }
+    },
     {
         id:18,
-        nombre:TOTEPRETTYBAGLADIESENBEIGE,
+        titulo:"TOTE",
         precio:15.000,
-        imagen:'../img/calzado1.png'
+        imagen:'./assets/img_productos/totebag.png',
+        categoria:{
+            nombre:"prendas",
+            id:"Prendas"
+        }
 
     }
-]
+];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelector(".botones-categoria");
+
+
+
+function cargarProductos(){
+     productos.forEach (producto =>{
+
+        const div= document.createElement ("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+        <div class="producto">
+            <img src="${producto.imagen}" alt="${producto.titulo}">
+            <h3>${producto.titulo}</h3>
+            <div class="pie_cont">
+            <h3>$${producto.precio}</h3>
+            <a href="index_carrito.html"><img id="${producto.id}" src="/assets/img/carrito logo.png" alt="carrito"></a>
+        </div>  
+    `
+        contenedorProductos.append(div);
+     })
+}
+
+cargarProductos();
